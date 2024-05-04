05b894a7436ac83ccf3d6b4400ff6a555987b02e
Raquel Natali <raquel.natali95@hotmail.com>
2024-05-04 14:05:14 -0300

    min_heap data structure problem

---
+++ dc_1492.py
@@ -0,0 +1,18 @@
+import heapq
+
+def min_rooms(intervals):
+    if not intervals:
+        return 0
+
+    intervals.sort(key=lambda x: x[0])
+
+    min_heap = []
+
+    for interval in intervals:
+        if min_heap and min_heap[0] <= interval[0]:
+            heapq.heappop(min_heap)
+        heapq.heappush(min_heap, interval[1])
+    
+    return len(min_heap)
+
+test_intervals = [(30, 75), (0, 50), (60, 150)]
